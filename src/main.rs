c7cf4d545ddb30c7685b91573f9f356afee9eda7
Eugene Palmoff <mahou@shoujo.pw>
2026-03-27 15:23:56 +0500

    Add cli args and improve networking with timeouts

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use clap::Parser;
 use futures_util::{
     SinkExt, StreamExt,
     stream::{SplitSink, SplitStream},
 };
-use log::{info, warn};
+use log::{debug, error, info, warn};
 use std::{
     error::Error,
     net::{IpAddr, SocketAddr},
@@ -11,87 +12,74 @@ use std::{
 };
 use tokio::{
     net::{TcpListener, TcpStream},
-    select,
-    sync::{
-        broadcast::{self, Sender as BroadcastSender, error::RecvError as BroadcastRecvError},
-        oneshot::{self, Receiver as OneshotReceiver, Sender as OneshotSender},
-    },
+    select, signal,
+    sync::broadcast::{Receiver, Sender, channel, error::RecvError},
     task::{JoinError, JoinSet},
-    time::sleep,
+    time::timeout,
 };
 use tokio_tungstenite::{WebSocketStream, tungstenite::Message};
 
-const HANDSHAKE_TIMEOUT: Duration = Duration::from_secs(10);
-
-const SEND_TIMEOUT: Duration = Duration::from_secs(2);
-
-const IDLE_TIMEOUT: Duration = Duration::from_secs(30);
-
-const CAPACITY: usize = 32768;
-
-const PORT: u16 = 3000;
+#[derive(Debug, Clone, Copy, Parser)]
+struct Args {
+    /// The addr to listen to
+    #[arg(short = 'd', long, default_value_t = SocketAddr::new(IpAddr::from([127, 0, 0, 1]), 8881))]
+    addr: SocketAddr,
+    /// Buffer capacity
+    #[arg(short, long, default_value_t = 32768)]
+    capacity: usize,
+    /// Handshake timeout in seconds
+    #[arg(long, default_value_t = 10)]
+    handshake_timeout: u64,
+    /// Send timeout in seconds
+    #[arg(long, default_value_t = 15)]
+    send_timeout: u64,
+    /// Idle timeout in seconds
+    #[arg(long, default_value_t = 30)]
+    idle_timeout: u64,
+}
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn Error>> {
-    simple_logger::init_with_env()?;
-    let (abort_tx, abort_rx) = oneshot::channel();
-    let (broadcast_tx, _) = broadcast::channel(CAPACITY);
-    set_shutdown_hook(abort_tx, broadcast_tx.clone());
-    let listener = TcpListener::bind(SocketAddr::new(IpAddr::from([0, 0, 0, 0]), PORT)).await?;
-    info!("listening on {PORT}");
+    let _ = simple_logger::init_with_env();
+    let args = Args::parse();
+    let listener = TcpListener::bind(args.addr).await?;
+    info!("listening on {addr}", addr = args.addr);
+    let (tx, _) = channel(args.capacity);
     let mut join_set = JoinSet::new();
-    serve(abort_rx, broadcast_tx, &listener, &mut join_set).await;
-    shutdown(&mut join_set).await;
-    Ok(())
-}
-
-fn set_shutdown_hook(abort_tx: OneshotSender<()>, broadcast_tx: BroadcastSender<Broadcast>) {
-    let _ = ctrlc::set_handler({
-        let mut once = Some((abort_tx, broadcast_tx));
-        move || {
-            if let Some((abort_tx, broadcast_tx)) = once.take() {
-                let _ = broadcast_tx.send(Broadcast {
-                    addr: None,
-                    message: Message::Close(None),
-                });
-                let _ = abort_tx.send(());
-            }
-        }
-    });
-}
-
-async fn serve(
-    mut abort_rx: OneshotReceiver<()>,
-    broadcast_tx: BroadcastSender<Broadcast>,
-    listener: &TcpListener,
-    join_set: &mut JoinSet<()>,
-) {
     loop {
-        tokio::select! {
-            _ = &mut abort_rx => {
+        select! {
+            _ = interruption() => {
                 break;
             }
             accept_result = listener.accept() => {
-                while let Some(join_result) = join_set.try_join_next() {
-                    rejoin(join_result);
-                }
+                clear_ready_tasks(&mut join_set);
                 match accept_result {
                     Ok((tcp_stream, addr)) => {
-                        join_set.spawn(accept(broadcast_tx.clone(), addr, tcp_stream));
+                        let accept_future = accept(args, tx.clone(), addr, tcp_stream);
+                        join_set.spawn(accept_future);
                     }
                     Err(error) => {
-                        warn!("accept error: {error}");
+                        error!("accept error: {error}");
                     }
                 }
-            },
+            }
         }
+    }
+    if !join_set.is_empty() {
+        info!("waiting for {} tasks before shutdown", join_set.len());
+        let _ = tx.send(Broadcast::Interruption);
+        clear_tasks(&mut join_set).await;
+    }
+    Ok(())
+}
+
+fn clear_ready_tasks(join_set: &mut JoinSet<()>) {
+    while let Some(join_result) = join_set.try_join_next() {
+        rejoin(join_result);
     }
 }
 
-async fn shutdown(join_set: &mut JoinSet<()>) {
-    if !join_set.is_empty() {
-        info!("waiting for {} tasks before shutdown", join_set.len());
-    }
+async fn clear_tasks(join_set: &mut JoinSet<()>) {
     while let Some(join_result) = join_set.join_next().await {
         rejoin(join_result);
     }
@@ -105,25 +93,51 @@ fn rejoin(result: Result<(), JoinError>) {
     }
 }
 
-async fn accept(broadcast_tx: BroadcastSender<Broadcast>, addr: SocketAddr, tcp_stream: TcpStream) {
+#[cfg(unix)]
+async fn interruption() -> Result<(), Box<dyn Error>> {
+    let mut hangup = signal::unix::signal(signal::unix::SignalKind::hangup())?;
+    let mut terminate = signal::unix::signal(signal::unix::SignalKind::terminate())?;
+    select! {
+        _ = signal::ctrl_c() => {},
+        _ = hangup.recv() => {},
+        _ = terminate.recv() => {},
+    }
+    Ok(())
+}
+
+#[cfg(not(unix))]
+async fn interruption() -> Result<(), Box<dyn Error>> {
+    signal::ctrl_c().await;
+    Ok(())
+}
+
+async fn accept(args: Args, tx: Sender<Broadcast>, addr: SocketAddr, tcp_stream: TcpStream) {
     info!("new connection: {addr}");
-    if let Some(ws_stream) = handshake(addr, tcp_stream).await {
-        listen(broadcast_tx, addr, ws_stream).await;
+    if let Some(ws_stream) = handshake(args, addr, tcp_stream).await {
+        listen(args, addr, tx, ws_stream).await;
     }
 }
 
-async fn handshake(addr: SocketAddr, tcp_stream: TcpStream) -> Option<WebSocketStream<TcpStream>> {
-    select! {
-        ws_result = tokio_tungstenite::accept_async(tcp_stream) => {
-             match ws_result {
-                Ok(ws_stream) => Some(ws_stream),
-                Err(error) => {
-                    warn!("handshake error with {addr}: {error}");
-                    None
-                }
-            }
-        },
-        _ = sleep(HANDSHAKE_TIMEOUT) => {
+async fn handshake(
+    args: Args,
+    addr: SocketAddr,
+    tcp_stream: TcpStream,
+) -> Option<WebSocketStream<TcpStream>> {
+    let handshake_result = timeout(
+        Duration::from_secs(args.handshake_timeout),
+        tokio_tungstenite::accept_async(tcp_stream),
+    )
+    .await;
+    match handshake_result {
+        Ok(Ok(ws_stream)) => {
+            debug!("handshake success with {addr}");
+            Some(ws_stream)
+        }
+        Ok(Err(error)) => {
+            error!("handshake error with {addr}: {error}");
+            None
+        }
+        Err(_) => {
             warn!("handshake timeout with {addr}");
             None
         }
@@ -131,92 +145,119 @@ async fn handshake(addr: SocketAddr, tcp_stream: TcpStream) -> Option<WebSocketS
 }
 
 async fn listen(
-    broadcast_tx: BroadcastSender<Broadcast>,
+    args: Args,
     addr: SocketAddr,
+    mut tx: Sender<Broadcast>,
     ws_stream: WebSocketStream<TcpStream>,
 ) {
-    let mut broadcast_rx = broadcast_tx.subscribe();
+    let mut rx = tx.subscribe();
     let (mut ws_sink, mut ws_source) = ws_stream.split();
     loop {
-        select! {
-            maybe_message = recv(addr, &mut ws_source) => {
-                match maybe_message {
-                    Some(message) if matches!(&message, Message::Close(_)) => {}
-                    Some(message) => {
-                        if broadcast_tx.send(Broadcast { addr: Some(addr), message }).is_err() {
-                            warn!("broadcast overload");
-                        }
-                    }
-                    None => {
-                        break;
-                    }
-                }
-            }
-            broadcast_result = broadcast_rx.recv() => {
-                match broadcast_result {
-                    Ok(broadcast) => {
-                        if broadcast.addr.is_none_or(|broadcast_addr| broadcast_addr != addr) {
-                            send(broadcast.message, addr, &mut ws_sink).await;
-                        }
-                    }
-                    Err(BroadcastRecvError::Lagged(_)) => {},
-                    Err(BroadcastRecvError::Closed) => {
-                        break;
-                    }
-                }
-            }
+        let must_abort = select! {
+            must_abort = broadcast(args,addr, &mut tx, &mut ws_source) => must_abort,
+            must_abort = relay(args, addr, &mut rx, &mut ws_sink) => must_abort,
+        };
+        if must_abort {
+            break;
         }
+    }
+}
+
+async fn broadcast(
+    args: Args,
+    addr: SocketAddr,
+    tx: &mut Sender<Broadcast>,
+    ws_source: &mut SplitStream<WebSocketStream<TcpStream>>,
+) -> bool {
+    let maybe_message = recv(args, addr, ws_source).await;
+    match maybe_message {
+        Some(message) if matches!(&message, Message::Close(_)) => false,
+        Some(message) => {
+            let _ = tx.send(Broadcast::Echo { addr, message });
+            false
+        }
+        None => true,
     }
 }
 
 async fn recv(
+    args: Args,
     addr: SocketAddr,
-    source: &mut SplitStream<WebSocketStream<TcpStream>>,
+    ws_source: &mut SplitStream<WebSocketStream<TcpStream>>,
 ) -> Option<Message> {
     loop {
-        select! {
-            maybe_message_result = source.next() => {
-                let Some(message_result) = maybe_message_result else {
-                    info!("closed {addr}");
-                    break None;
-                };
-                match message_result {
-                    Ok(message) if matches!(&message, Message::Close(_)) => {}
-                    Ok(message) => {
-                        break Some(message);
-                    }
-                    Err(error) => {
-                        warn!("recv error with {addr}: {error}");
-                    }
-                }
+        let recv_result = timeout(Duration::from_secs(args.idle_timeout), ws_source.next()).await;
+        break match recv_result {
+            Ok(Some(Ok(message))) if matches!(&message, Message::Close(_)) => {
+                continue;
             }
-            _ = sleep(IDLE_TIMEOUT) => {
+            Ok(Some(Ok(message))) => Some(message),
+            Ok(Some(Err(error))) => {
+                error!("recv error with {addr}: {error}");
+                continue;
+            }
+            Ok(None) => {
+                info!("closed {addr}");
+                None
+            }
+            Err(_) => {
                 warn!("idle timeout with {addr}");
-                break None;
+                None
             }
+        };
+    }
+}
+
+async fn relay(
+    args: Args,
+    addr: SocketAddr,
+    rx: &mut Receiver<Broadcast>,
+    ws_sink: &mut SplitSink<WebSocketStream<TcpStream>, Message>,
+) -> bool {
+    let broadcast_result = rx.recv().await;
+    match broadcast_result {
+        Ok(Broadcast::Echo {
+            addr: broadcast_addr,
+            message,
+        }) => {
+            if addr != broadcast_addr {
+                send(args, addr, message, ws_sink).await;
+            }
+            false
         }
+        Ok(Broadcast::Interruption) => {
+            send(args, addr, Message::Close(None), ws_sink).await;
+            true
+        }
+        Err(RecvError::Lagged(_)) => false,
+        Err(RecvError::Closed) => true,
     }
 }
 
 async fn send(
-    message: Message,
+    args: Args,
     addr: SocketAddr,
+    message: Message,
     ws_sink: &mut SplitSink<WebSocketStream<TcpStream>, Message>,
 ) {
-    select! {
-        send_result = ws_sink.send(message) => {
-            if let Err(error) = send_result {
-                warn!("send error with {addr}: {error}");
-            }
-        },
-        _ = sleep(SEND_TIMEOUT) => {
+    let send_result = timeout(
+        Duration::from_secs(args.send_timeout),
+        ws_sink.send(message),
+    )
+    .await;
+    match send_result {
+        Ok(Ok(())) => {}
+        Ok(Err(error)) => {
+            error!("send error with {addr}: {error}");
+        }
+        Err(_) => {
             warn!("send timeout with {addr}");
         }
-    };
+    }
 }
 
 #[derive(Debug, Clone)]
-struct Broadcast {
-    addr: Option<SocketAddr>,
-    message: Message,
+enum Broadcast {
+    Echo { addr: SocketAddr, message: Message },
+    Interruption,
 }
